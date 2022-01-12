4ead2265ba0f6eb55a6308ae0640ef86c8c62936
Myroniuk, Volodymyr <Volodymyr.Myroniuk@onseo.biz>
2022-01-12 16:34:48 +0200

    Add custom printer

---
+++ custom_printer.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include "custom_printer.h"
+
+void custom_print() {
+    printf("CUSTOM PRINTER\n");
+}
---
+++ custom_printer.h
@@ -0,0 +1,6 @@
+#ifndef CUSTOM_PRINTER_H
+#define CUSTOM_PRINTER_H
+
+void custom_print();
+
+#endif
